6db83fc2b56d7b92d78cc40347f9bea9237b9e84
dayoung <ldy8536@gmail.com>
2022-12-29 15:56:36 +0900

    Initial commit

---
+++ src/main/java/com/example/dowitter/DowitterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dowitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dowitter/DowitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dowitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
